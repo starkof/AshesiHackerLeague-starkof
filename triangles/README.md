9e205f2d602255ee3926520e845bed43812b6a30
starkof <saystephan@gmail.com>
2018-09-24 16:16:20 +0000

    Wrote solution to triangles problem

---
+++ triangles/README.md
@@ -0,0 +1,5 @@
+# Run the program by typing
+
+python3 sum_of_triangles.py <number>
+
+Note that the program should be run with python 3.x
---
+++ triangles/sum_of_triangles.py
@@ -0,0 +1,21 @@
+# Sum of triangles
+# by: Stephan N. Ofosuhene
+
+import sys
+
+
+def triangles(n):
+    if n < 1:
+        return 0
+
+    t = 0
+    for i in range(1, n+1):
+        t += (i*(i + 1))/2
+
+    return t
+
+
+# print(triangles(2309))
+
+if __name__ == '__main__':
+    print(triangles(int(sys.argv[1])))
